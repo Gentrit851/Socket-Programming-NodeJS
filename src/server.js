a755ac3a691248e0215c9fafec3bb109f00808ec
Fortesa Cena <fortesa.cena@student.uni-pr.edu>
2023-11-28 22:15:10 +0100

    Add-message-handling-and-response-logic-in-server

--- src/server.js
+++ src/server.js
@@ -8,3 +8,19 @@ server.on('listening', () => {
     const address = server.address()
     console.log('Listining to ', 'Address: ', address.address, 'Port: ', address.port)
 })
+
+server.on('message', (message, info) => {
+    console.log('Message', message.toString())
+  
+    const response = Buffer.from('Message Received')
+  
+    server.send(response, info.port, info.address, (err) => {
+      if (err) {
+        console.error('Failed to send response !!')
+      } else {
+        console.log('Response send Successfully')
+      }
+    })
+  })
+  
+  server.bind(port)
