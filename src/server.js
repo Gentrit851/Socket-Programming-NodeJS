822c4bd52f7e6829697bdc5c1f3c2c2e618bbc5d
Gentrit <gentikryeziu851@gmail.com>
2023-11-30 20:03:46 +0100

    add-client-to-client-array-if-not-stop-command
    Adds the Client to the clients array. If the command that they sent is 'stop' then remove the client from the array.

--- src/server.js
+++ src/server.js
@@ -4,23 +4,37 @@ const server = UDP.createSocket('udp4')
 
 const port = 2222
 
+const clients = [];
+
+server.on('message', (message, remote) => {
+  const messageString = message.toString();
+  console.log(`Received message from ${remote.address}:${remote.port}: ${messageString}`);
+
+  if (command.toLowerCase() === 'stop') {
+
+    const index = clients.findIndex(client => client.address === remote.address && client.port === remote.port);
+    if (index !== -1) {
+      clients.splice(index, 1);
+      console.log(`Client ${remote.address}:${remote.port} has disconnected.`);
+      console.log(clients);
+    }
+  }
+
+  else {
+    const existingClient = clients.find(client => client.address === remote.address && client.port === remote.port);
+
+    if (!existingClient) {
+      clients.push({ address: remote.address, port: remote.port });
+      console.log(`New client connected: ${remote.address}:${remote.port}`);
+      console.log(clients);
+    }
+  }
+});
+
 server.on('listening', () => {
-    const address = server.address()
-    console.log('Listining to ', 'Address: ', address.address, 'Port: ', address.port)
+  const address = server.address()
+  console.log('Listining to ', 'Address: ', address.address, 'Port: ', address.port)
 })
 
-server.on('message', (message, info) => {
-    console.log('Message', message.toString())
-  
-    const response = Buffer.from('Message Received')
-  
-    server.send(response, info.port, info.address, (err) => {
-      if (err) {
-        console.error('Failed to send response !!')
-      } else {
-        console.log('Response send Successfully')
-      }
-    })
-  })
-  
-  server.bind(port)
+
+server.bind(port)
